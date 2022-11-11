ce415ed278dc3127ab9809507ba89c2ae677894d
jupl <juan.placencia.512@gmail.com>
2022-11-10 23:08:18 -0600

    Fix typo

--- lint/configurations/next.ts
+++ lint/configurations/next.ts
@@ -3,6 +3,6 @@ import {moduleConfiguration} from '../util'
 /**
  * Next.js related configuration.
  */
-export const configuration$a = moduleConfiguration('eslint-plugin-next', {
+export const configuration = moduleConfiguration('eslint-plugin-next', {
   extends: ['next/core-web-vitals'],
-});
+})
